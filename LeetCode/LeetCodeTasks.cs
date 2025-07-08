a00acb53e3afa96114ebe99123ed4b02dedeeea2
Lutered <oy3gind@gmail.com>
2025-07-08 14:48:36 +0300

    IsPalindrome

--- LeetCode/LeetCodeTasks.cs
+++ LeetCode/LeetCodeTasks.cs
@@ -1424,6 +1424,34 @@ namespace LeetCode
             return resultString;
         }
 
+        public bool IsPalindrome(string s)
+        {
+            int i = 0,
+                j = s.Length - 1;
+
+            while (i < j)
+            {
+                if (!char.IsLetterOrDigit(s[i]))
+                {
+                    i++;
+                    continue;
+                }
+
+                if (!char.IsLetterOrDigit(s[j]))
+                {
+                    j--;
+                    continue;
+                }
+
+                if (Char.ToLower(s[i]) != Char.ToLower(s[j])) return false;
+
+                i++;
+                j--;
+            }
+
+            return true;
+        }
+
         #region Roman
 
         private Dictionary<char, int> romanValuesDict = new Dictionary<char, int>
